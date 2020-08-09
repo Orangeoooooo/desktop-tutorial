7799a42cc78138291a7db06343f6beba368c837d
yanqza <yanquanzhou010@163.com>
2020-08-09 22:53:59 +0800

    github第二行代码

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("github第一行代码");
+        System.out.println("github第二行代码");
     }
 }
